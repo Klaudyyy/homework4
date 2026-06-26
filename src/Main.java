68d8a58a664d0e82789fba6d1659deac562bcc44
Пользователь <@https://github.com/Klaudyyy/helloworld.git>
2026-06-26 20:29:53 +0300

    revision

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,14 @@ public class Main {
             System.out.println("Если возраст человека равен " + age + " он совершеннолетний");
         } else {
             System.out.println("Если возраст человека равен " + age + " он не достиг совершеннолетия, нужно подождать");
+        }
             int temperature = 6;
             if (temperature < 5) {
                 System.out.println("На улице холодно, нужно надеть шапку");
             } else {
                 System.out.println("Сегодня тепло, можно идти без шапки");
             }
+
             int speed = 75;
             if (speed > 60) {
                 System.out.println("Если скорость " + speed + " , то придется заплатить штраф");
@@ -21,10 +23,10 @@ public class Main {
             }
 
             int age2 = 27;
-            if (age2 > 2 && age2 < 6) {
+            if (age2 > 1 && age2 < 7) {
                 System.out.println("Если возраст человека равен " + age2 + ", то ему нужно ходить в детский сад");
             }
-            if (age2 > 7 && age2 < 17) {
+            if (age2 > 7 && age2 < 18) {
                 System.out.println("Если возраст человека равен " + age2 + ", то ему нужно ходить в школу");
             }
             if (age2 >= 18 && age2 <= 24) {
@@ -37,7 +39,7 @@ public class Main {
             int age3 = 15;
             if (age3 < 5) {
                 System.out.println("Если возраст ребенка равен " + age3 + ", то ему нельзя кататься на аттракционе");
-            }  if ( age3 > 5 && age3 <= 14) {
+            }  if ( age3 > 4 && age3 <= 14) {
                 System.out.println("Если возраст ребенка равен " + age3 + ", то ему можно кататься на аттракционе только в сопровождении взрослого");
             } if (age3 > 14) {
                 System.out.println("Если возраст ребенка равен " + age3 + ", то ему можно кататься на аттракционе без сопровождения взрослого");
@@ -46,13 +48,13 @@ public class Main {
             int place = 107;
             if (place <60) {
                 System.out.println("Сидячие места есть");
-            } if (place >=60 && place < 102) {
+            } if (place >=60 && place < 103) {
                 System.out.println("Стоячие места есть");
             } if (place >102) {
                 System.out.println("Мест нет");
             }
         }
 }
-}
+
 
 
